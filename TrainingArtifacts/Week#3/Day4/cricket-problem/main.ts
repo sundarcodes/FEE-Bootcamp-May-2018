68ef9780c94f220b375737039326f5c1460ed127
sundarcodes <sundar82@gmail.com>
2018-06-12 11:24:53 +0530

    Added webpack config for cricket scoring app

--- TrainingArtifacts/Week#3/Day4/cricket-problem/main.ts
+++ TrainingArtifacts/Week#3/Day4/cricket-problem/main.ts
@@ -7,3 +7,7 @@ scorer.addBatsman(batsman1);
 scorer.addBatsman(batsman2);
 scorer.calculateScore([1, 2, 0, 0, 4, 1, 6, 2, 1, 3]);
 scorer.printScore();
+
+// Here I need to update the para element in the dom
+
+document.getElementById('totalScore').innerHTML = scorer.totalScore + '';
---
+++ TrainingArtifacts/Week#3/Day4/cricket-problem/webpack.config.js
@@ -0,0 +1,8 @@
+const config = {
+    entry: './dist/main.js',
+    output: {
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
