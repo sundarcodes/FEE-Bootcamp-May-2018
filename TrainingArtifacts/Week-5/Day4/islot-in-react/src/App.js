77f42d5908a8d8f70fd0a262acf40bef319334d0
sundarcodes <sundar82@gmail.com>
2018-06-21 16:54:58 +0530

    Added React files for iSlot app

---
+++ TrainingArtifacts/Week-5/Day4/islot-in-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
